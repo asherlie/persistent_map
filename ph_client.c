70a7749346dfcf64c0343b756aabbd90c4385eaa
asherlie <lieber.asher@gmail.com>
2022-04-14 18:18:06 -0400

    implemented insert_data(), added example use to main()

--- ph_client.c
+++ ph_client.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -8,6 +9,10 @@
 int establish_conn(char* ip){
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in peer_addr = {0};
+    
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+    setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
+
     peer_addr.sin_port = PORT;
     peer_addr.sin_family = AF_INET;
     inet_aton(ip, &peer_addr.sin_addr);
@@ -30,13 +35,28 @@ int new_map(char* ip){
     msg.act = CREATE_MAP;
 
     peer_sock = establish_conn(ip);
+    if(peer_sock == -1)return -1;
     write_msg(peer_sock, &msg);
     msg.int_value = -1;
     if(!recv_msg(peer_sock, &msg))return -1;
     return msg.int_value;
 }
 
-_Bool insert_data(){
+_Bool insert_data(char* ip, int map_id, void* key, int key_len, void* data, int data_len, int int_value){
+    struct ph_msg msg = {0};
+    int peer_sock;
+    msg.act = INSERT_PH_KEY_VALUE;
+    msg.map_id = map_id;
+    msg.data_key_len = key_len;
+    msg.data_value_len = data_len;
+    msg.int_value = int_value;
+    msg.data_key = key;
+    msg.data_value = data;
+    
+    peer_sock = establish_conn(ip);
+    write_msg(peer_sock, &msg);
+    if(!recv_msg(peer_sock, &msg))return -1;
+    return msg.int_value;
 }
 
 struct ph_msg* lookup_data(){
@@ -46,4 +66,9 @@ _Bool remove_data(){
 }
 
 int main(){
+    char ip[] = "192.168.86.21";
+    char key[] = "/home/asher/somefile.extension";
+    int map_id = 0;
+    /*printf("new map created with id: %i\n", (map_id = new_map(ip)));*/
+    printf("insert_data(): %i\n", insert_data(ip, map_id, key, sizeof(key), NULL, 0, 11));
 }
