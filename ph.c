43c07292c52df1a3c0f8a880afdbc4e37774824d
asherlie <lieber.asher@gmail.com>
2022-04-13 23:15:00 -0400

    init_ph() now sets dump_fn, which is used to record commands that are issued in order to replay them on startup and rebuild a map, implemented perform_msg_action_ph(), which takes in a struct ph_msg and performs a ph action, added restore_ph(), which loads a ph from dump_fn

--- ph.c
+++ ph.c
@@ -2,7 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
+#include "msg.h"
 #include "ph.h"
 
 #if 0
@@ -29,7 +33,8 @@ void init_hm(struct hm* map, int n_buckets){
 	map->buckets = calloc(sizeof(struct hm_entry*), n_buckets);
 }
 
-void init_ph(struct persistent_hash* ph){
+void init_ph(struct persistent_hash* ph, char* dump_fn){
+    ph->dump_fn = dump_fn;
 	ph->n_maps = 0;
 	ph->cap = 100;
 	ph->maps = malloc(sizeof(struct hm*)*ph->cap);
@@ -121,6 +126,72 @@ _Bool remove_ph(struct persistent_hash* ph, int id, void* data_key, int len){
     e = NULL;
 
     return 1;
+}
+
+// each msg should send a response int for success, and an optional response buffer
+_Bool perform_msg_action_ph(struct persistent_hash* ph, struct ph_msg* pm, int peer_sock){
+/*enum action {CREATE_MAP, INSERT_PH_KEY_VALUE, LOOKUP_ENTRY, REMOVE_PH};*/
+    struct ph_msg resp = {0};
+    /*int resp = -1;*/
+    switch(pm->act){
+        case CREATE_MAP:
+            resp.int_value = create_map(ph);
+            break;
+        case INSERT_PH_KEY_VALUE:
+            resp.int_value = insert_ph_key_value(ph, pm->map_id, pm->data_key, pm->data_key_len, pm->data_value, pm->int_value);
+            break;
+        case LOOKUP_ENTRY:
+            // TODO: how do i return an entry
+            // AH! with write_msg()
+            // the receiver will just use a recv_msg() that i'll define it returns a populated msg
+            break;
+        case REMOVE_PH:
+            remove_ph(ph, pm->map_id, pm->data_key, pm->data_key_len);
+            break;
+    }
+
+    /* TODO: could call to htonl() make resp negative? */
+    /* if a response is neccessary, send
+     * peer will keep connection open until they recv a response
+     */
+    if(peer_sock != -1){
+        write_msg(peer_sock, &resp);
+    }
+    return resp.int_value >= 0;
+}
+
+/* to be called on startup to restore dumped values */
+/*should this fn be here or in the thread that reads msgs*/
+/*
+ * it's good here actually, we'll just call it after we init or in init
+ * we'll go through all entries in file, loading into temp msg
+ * then call perform_msg_action_ph()
+ * to rebuild or map
+ * 
+ * the server thread will call the very same perform_msg_action_ph() to handle messages as they arrive
+ * hmm, should these messages actually just be added to the queue on startup and handled normally?
+ * i think this should be the approach
+ * so we never have any edge cases, just read from file, insert to queue, business as usual
+ * might not be as safe though because messages might come in the way
+ * 
+ * unless we don't set up networkign until after this
+ * easier actually to just have the code separated
+*/
+
+/*okay, we have a working restore(), now just need to append_dump() each time a message is popped from the queue in ph_server*/
+int restore_ph(struct persistent_hash* ph){
+    int entries_restored = 0;
+    int fd  = open(ph->dump_fn, O_RDONLY);
+    struct ph_msg msg;
+    if(fd < 0)return -1;
+
+    while(recv_msg(fd, &msg)){
+        printf("performing action %i\n", msg.act);
+        perform_msg_action_ph(ph, &msg, -1);
+    }
+
+    close(fd);
+    return entries_restored;
 }
 
 void print_maps(struct persistent_hash* ph){
