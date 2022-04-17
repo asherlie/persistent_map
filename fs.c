aaf39ba84da7dc0c7ae595cac176cbf043b74147
asherlie <lieber.asher@gmail.com>
2022-04-17 19:15:14 -0400

    added [l]ist command for fs

--- fs.c
+++ fs.c
@@ -4,21 +4,34 @@
 #include "msg.h"
 #include "ph_client.h"
 
-/* ./fs <u|d> file <address> [write_fn] */
+
+/* ./fs <u|d|l> <address> <fn> [write_fn] */
 int main(int a, char** b){
-    if(a < 4)return 0;
+    char** lst, * ip = b[2];
+    int len;
     switch(tolower(*b[1])){
         case 'u':
+            if(a < 4)return 0;
             /* creating a map with index 0 if one doesn't exist */
-            new_map_conditional(b[3], 1);
-            if(upload_file(b[3], 0, b[2]))
+            new_map_conditional(ip, 1);
+            /* TODO: uploading very large files doesn't work - find out why */
+            if(upload_file(ip, 0, b[3]))
                 puts("succesfully uploaded file");
             else puts("failed to upload file");
             break;
         case 'd':
-            if(download_file(b[3], 0, b[2], (a > 4) ? b[4] : b[2]))
-                printf("downloaded file to \"%s\"\n", (a > 4) ? b[4] : b[2]);
+            if(a < 4)return 0;
+            if(download_file(ip, 0, b[3], (a > 4) ? b[4] : b[3]))
+                printf("downloaded file to \"%s\"\n", (a > 4) ? b[4] : b[3]);
             else puts("failed to download file");
+            break;
+        case 'l':
+            if(a < 3)return 0;
+            lst = list_str_keys(ip, 0, &len);
+            if(!lst)break;
+            for(int i = 0; i < len; ++i){
+                printf("\"%s\"\n", lst[i]);
+            }
             break;
     }
 
