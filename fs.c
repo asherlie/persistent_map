63a82a680e55b6c690352d659311e0d43d7cdb8d
asherlie <lieber.asher@gmail.com>
2022-04-17 19:22:53 -0400

    added some error handling and helpful output - order of args is now more sensible

--- fs.c
+++ fs.c
@@ -5,13 +5,20 @@
 #include "ph_client.h"
 
 
-/* ./fs <u|d|l> <address> <fn> [write_fn] */
+/* ./fs <address> <u|d|l> <fn> [write_fn] */
 int main(int a, char** b){
-    char** lst, * ip = b[2];
+    char** lst, * ip = b[1];
     int len;
-    switch(tolower(*b[1])){
+    if(a < 3){
+        puts("an action must be provided");
+        return 0;
+    }
+    switch(tolower(*b[2])){
         case 'u':
-            if(a < 4)return 0;
+            if(a < 4){
+                puts("a file must be provided");
+                return 0;
+            }
             /* creating a map with index 0 if one doesn't exist */
             new_map_conditional(ip, 1);
             /* TODO: uploading very large files doesn't work - find out why */
@@ -20,19 +27,23 @@ int main(int a, char** b){
             else puts("failed to upload file");
             break;
         case 'd':
-            if(a < 4)return 0;
+            if(a < 4){
+                puts("a file must be provided");
+                return 0;
+            }
             if(download_file(ip, 0, b[3], (a > 4) ? b[4] : b[3]))
                 printf("downloaded file to \"%s\"\n", (a > 4) ? b[4] : b[3]);
             else puts("failed to download file");
             break;
         case 'l':
-            if(a < 3)return 0;
             lst = list_str_keys(ip, 0, &len);
             if(!lst)break;
             for(int i = 0; i < len; ++i){
                 printf("\"%s\"\n", lst[i]);
             }
             break;
+        default:
+            puts("action not recognized");
     }
 
     return 1;
